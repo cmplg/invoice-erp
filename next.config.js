69c7b76ccaab9299c44a4f5be5be1aa6f6a0097f
cmplg <cmplg@github.com>
2026-05-16 16:41:53 +0700

    Replace Next.js config with next.config.js

---
+++ next.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  allowedDevOrigins: ['10.201.22.252'],
+};
+
+module.exports = nextConfig;
--- next.config.ts
+++
@@ -1,7 +0,0 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  allowedDevOrigins: ['10.201.22.252'],
-};
-
-export default nextConfig;
